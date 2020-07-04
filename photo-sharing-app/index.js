d591b89288b7fcb99cd29b30364fe856ca675abd
LiamOSullivan <lmosulli@tcd.ie>
2020-07-04 16:41:43 +0100

    add container creation

--- photo-sharing-app/index.js
+++ photo-sharing-app/index.js
@@ -1,7 +1,35 @@
 #!/usr/bin/env node
+require('dotenv').config()
+const util = require('util')
+const storage = require('azure-storage')
 
-function main () {
-  console.log('Hello, World!')
+const containerName = 'photoblobs'
+
+const blobService = storage.createBlobService()
+const createContainerAsync = util.promisify(blobService.createContainerIfNotExists).bind(blobService)
+const uploadBlobAsync = util.promisify(blobService.createBlockBlobFromLocalFile).bind(blobService)
+
+async function main () {
+  try {
+    // This makes an actual service call to the Azure Storage service.
+    // Unless this call fails, the container will have been created.
+    let creationResult = await createContainerAsync(containerName)
+
+    if (creationResult.created) {
+      console.log(`conatiner ${containerName} created`)
+    } else {
+      console.log(`conatiner ${containerName} already exists`)
+    }
+    // This transfers data in the file to the blob on the service.
+    // let uploadResult = await uploadBlobAsync(containerName, 'myphoto', 'photo.png')
+    // if (uploadResult.created) {
+    //   console.log('blob uploaded')
+    // } else {
+    //   console.log('blob already exists')
+    // }
+  } catch (err) {
+    console.log(err.message)
+  }
 }
 
 main()
